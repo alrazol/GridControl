f631b24b7974920cfb4a6fa395948cd321b571b6
Alfred Razol <alfred.razol01@gmail.com>
2025-10-21 11:35:54 +0200

    chore: rollback debug

--- src/core/infrastructure/settings.py
+++ src/core/infrastructure/settings.py
@@ -3,11 +3,6 @@ from pathlib import Path
 from dotenv import find_dotenv
 from pydantic_settings import BaseSettings
 
-print(find_dotenv(".env"))
-import os
-
-print(os.getcwd())
-
 dotenv.load_dotenv(find_dotenv(".env"))
 
 
